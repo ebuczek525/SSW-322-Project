3e85d6c7a411fe3a1f375ac8bd55a7ae175f3f06
Shinyships <xxandynguyenxx@gmail.com>
2020-03-29 12:39:02 -0400

    website doesn't look like hot garbage

--- pages/display.js
+++ pages/display.js
@@ -1,14 +1,24 @@
 import Layout from '../components/MyLayout';
+import { 
+   Select,
+   Button,
+   Input,
+ } from 'antd';
+
+import '../styling/display.less';
+
 
 export default function Display() {
 return (
 <div>
    <Layout>
-      <p>Please enter code to display test/survey:</p>
-      <form>
-         <input type="text" name="ID_code" />
-         <input type="submit" value="Submit" />
-      </form>
+      <div className='display'>
+      <div>Please enter code to display test/survey:</div>
+         <Input id='idInput' placeholder="MongoDB ID" />
+         <Button type="primary" style={{marginLeft: '.5vw', marginTop: '.5vh',  width: '135px'}}>Submit</Button>
+      </div>
+      <div className='displayContents'>
+      </div>
    </Layout>
 </div>
 );
