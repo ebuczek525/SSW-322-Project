40cf9e984d0523d7d1fa978f313af9b444ea832e
Shinyships <xxandynguyenxx@gmail.com>
2020-03-29 14:19:41 -0400

    functions are now classes so state is usable

--- pages/create.js
+++ pages/create.js
@@ -1,68 +1,77 @@
 import Layout from "../components/MyLayout";
-import Link from "next/link";
 
 import { Select, Button, Input, Switch } from "antd";
 import "../styling/create.less";
 
-const linkStyle = {
-  marginRight: 15
-};
 
 function handleChange(value) {
   console.log(`selected ${value}`);
 }
 
 const { Option } = Select;
-const { TextArea } = Input;
 
-export default function Create() {
-  return (
-    <div>
-      <Layout>
-        <div className="create">
-          <div id="createInput">
-            <div style={{marginBottom: '0.5vh'}}>Test or Survey:</div>
-            <Switch
-              checkedChildren="Test"
-              unCheckedChildren="Survey"
-              defaultChecked
-              style={{ width: "4.5vw", }}
-            />
-          </div>
-          <div id="createInput">
-            <div>Test Name:</div>
-            <Input placeholder="Test Name" />
-          </div>
-          <div id="description">
-            <div >Add a Description:</div>
-            <Input />
-          </div>
-          <div style={{marginLeft: '2vw', marginTop: "1vh"}}>
-            <div style={{display:'inline-block'}}>
-                <div>Add a Question:</div>
-              <div>
-                <Select defaultValue="Multiple Choice" onChange={handleChange}>
-                  <Option value="mc">Multiple Choice</Option>
-                  <Option value="tf">True/False</Option>
-                  <Option value="sa">Short Answer</Option>
-                  <Option value="es">Essay</Option>
-                  <Option value="mt">Matching</Option>
-                  <Option value="rc">Ranked Choice</Option>
-                </Select>
-                <Button shape="circle" icon="plus" style={{ marginLeft: ".5vw" }} />
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    };
+  }
+
+  render () {
+    return (
+      <>
+        <div>
+          <Layout>
+            <div className="create">
+              <div id="createInput">
+                <div style={{marginBottom: '0.5vh'}}>Test or Survey:</div>
+                <Switch
+                  checkedChildren="Test"
+                  unCheckedChildren="Survey"
+                  defaultChecked
+                  style={{ width: "4.5vw", }}
+                />
               </div>
-            </div>
-            <div style={{display:'inline-block'}}>
-              <Button type="primary" style={{ marginLeft: "2vw", width: "135px" }}>
-                Create
-              </Button>
-            </div>
-          </div>
-        </div>   
-        <div id="createInput"></div>
-        <hr style={{ width: "97%", marginTop: "1vh", marginBottom: "2vh" }} />
-        <div className="createContents"></div>
-      </Layout>
-    </div>
-  );
+              <div id="createInput">
+                <div>Test Name:</div>
+                <Input placeholder="Test Name" />
+              </div>
+              <div id="description">
+                <div >Add a Description:</div>
+                <Input />
+              </div>
+              <div style={{marginLeft: '2vw', marginTop: "1vh"}}>
+                <div style={{display:'inline-block'}}>
+                    <div>Add a Question:</div>
+                  <div>
+                    <Select defaultValue="Multiple Choice" onChange={handleChange}>
+                      <Option value="mc">Multiple Choice</Option>
+                      <Option value="tf">True/False</Option>
+                      <Option value="sa">Short Answer</Option>
+                      <Option value="es">Essay</Option>
+                      <Option value="mt">Matching</Option>
+                      <Option value="rc">Ranked Choice</Option>
+                    </Select>
+                    <Button shape="circle" icon="plus" style={{ marginLeft: ".5vw" }} />
+                  </div>
+                </div>
+                <div style={{display:'inline-block'}}>
+                  <Button type="primary" style={{ marginLeft: "2vw", width: "135px" }}>
+                    Create
+                  </Button>
+                </div>
+              </div>
+            </div>   
+            <div id="createInput"></div>
+            <hr style={{ width: "97%", marginTop: "1vh", marginBottom: "2vh" }} />
+            <div className="createContents"></div>
+          </Layout>
+        </div>
+      </>
+    );
+  }
 }
+  
+export default Create;
--- pages/display.js
+++ pages/display.js
@@ -1,6 +1,5 @@
 import Layout from '../components/MyLayout';
 import { 
-   Select,
    Button,
    Input,
  } from 'antd';
@@ -8,18 +7,32 @@ import {
 import '../styling/display.less';
 
 
-export default function Display() {
-return (
-<div>
-   <Layout>
-      <div className='display'>
-      <div>Please enter code to display test/survey:</div>
-         <Input id='idInput' placeholder="MongoDB ID" />
-         <Button type="primary" style={{marginLeft: '.5vw', marginTop: '.5vh',  width: '135px'}}>Submit</Button>
-      </div>
-      <div className='displayContents'>
-      </div>
-   </Layout>
-</div>
-);
+class Display extends React.Component {
+   constructor(props) {
+     super(props);
+ 
+     this.state = {
+       
+     };
+   }
+ 
+   render () {
+      return (
+         <>
+            <div>
+               <Layout>
+                  <div className='display'>
+                  <div>Please enter code to display test/survey:</div>
+                     <Input id='idInput' placeholder="MongoDB ID" />
+                     <Button type="primary" style={{marginLeft: '.5vw', marginTop: '.5vh',  width: '135px'}}>Submit</Button>
+                  </div>
+                  <div className='displayContents'>
+                  </div>
+               </Layout>
+            </div>
+         </>
+      );
+   }
 }
+
+export default Display;
